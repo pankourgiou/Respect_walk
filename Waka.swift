155f65438dd4d733c52e95138a8f57b58a54f0df
L <60981745+pankourgiou@users.noreply.github.com>
2025-01-05 21:39:46 +0200

    Update and rename Walk.py to Waka.swift

---
+++ Waka.swift
@@ -0,0 +1,2 @@
+#haha Shakira
+print("waka waka")
--- Walk.py
+++
@@ -1,2 +0,0 @@
-#Walk and respect we don't just walk....I am repecting and thankful when I walk....It's a command Respect, walk.
-print("Rescpect, walk")
